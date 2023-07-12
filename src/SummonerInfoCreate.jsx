7a01a76b5f99ef709d130359279c10dd112901b1
Paolo Maloles <jpaolomaloles@gmail.com>
2023-07-12 15:58:20 -0500

    added title to SummonerInfoCreate

--- src/SummonerInfoCreate.jsx
+++ src/SummonerInfoCreate.jsx
@@ -6,32 +6,35 @@ export function SummonerInfoCreate(props) {
   };
 
   return (
-    <form onSubmit={handleOnSubmit}>
-      <div>
-        puuid: <input name="puuid" type="text"></input>
-      </div>
-      <div>
-        tier: <input name="tier" type="text"></input>
-      </div>
-      <div>
-        rank: <input name="rank" type="text"></input>
-      </div>
-      <div>
-        league_points: <input name="league_points" type="text"></input>
-      </div>
-      <div>
-        riot_summoner_id: <input name="riot_summoner_id" type="text"></input>
-      </div>
-      <div>
-        summoner_name: <input name="summoner_name" type="text"></input>
-      </div>
-      <div>
-        wins: <input name="wins" type="text"></input>
-      </div>
-      <div>
-        losses: <input name="losses" type="text"></input>
-      </div>
-      <button type="submit">Create Summoner Info </button>
-    </form>
+    <div>
+      <h1> Create Summoner Info </h1>
+      <form onSubmit={handleOnSubmit}>
+        <div>
+          puuid: <input name="puuid" type="text"></input>
+        </div>
+        <div>
+          tier: <input name="tier" type="text"></input>
+        </div>
+        <div>
+          rank: <input name="rank" type="text"></input>
+        </div>
+        <div>
+          league_points: <input name="league_points" type="text"></input>
+        </div>
+        <div>
+          riot_summoner_id: <input name="riot_summoner_id" type="text"></input>
+        </div>
+        <div>
+          summoner_name: <input name="summoner_name" type="text"></input>
+        </div>
+        <div>
+          wins: <input name="wins" type="text"></input>
+        </div>
+        <div>
+          losses: <input name="losses" type="text"></input>
+        </div>
+        <button type="submit">Create Summoner Info </button>
+      </form>
+    </div>
   );
 }
