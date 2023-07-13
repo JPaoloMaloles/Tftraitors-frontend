d44b6e61306fbb962842967c0d4866fe9296a105
Paolo Maloles <jpaolomaloles@gmail.com>
2023-07-13 12:45:54 -0500

    reverse mapping for Match display in SummonerInfoIndex

--- src/SummonerInfoIndex.jsx
+++ src/SummonerInfoIndex.jsx
@@ -28,31 +28,33 @@ export function SummonerInfoIndex(props) {
                   <p></p>
 
                   <Accordion>
-                    {summonerInfo.match_summoner_performances.map((match_summoner_performance, index) => (
-                      <div key={match_summoner_performance.id}>
-                        <Accordion>
-                          <Accordion.Item eventKey={index}>
-                            <Accordion.Header> Match #{index + 1} </Accordion.Header>
-                            <Accordion.Body>
-                              <p> match id: {match_summoner_performance.match_id} </p>
-                              <p> summoner_info_id: {match_summoner_performance.summoner_info_id} </p>
-                              <p> riot_match_id: {match_summoner_performance.riot_match_id} </p>
-                              <p> puuid: {match_summoner_performance.puuid} </p>
-                              <p> gold_left: {match_summoner_performance.gold_left} </p>
-                              <p> last_round: {match_summoner_performance.last_round} </p>
-                              <p> level_placement: {match_summoner_performance.level_placement} </p>
-                              <p> players_eliminated: {match_summoner_performance.players_eliminated} </p>
-                              <p> time_eliminated: {match_summoner_performance.time_eliminated} </p>
-                              <p> total_damage_to_players: {match_summoner_performance.total_damage_to_players} </p>
-                              <p> first_augment: {match_summoner_performance.first_augment} </p>
-                              <p> second_augment: {match_summoner_performance.second_augment} </p>
-                              <p> third_augment: {match_summoner_performance.third_augment} </p>
-                              <p> companion_id: {match_summoner_performance.companion_id} </p>
-                            </Accordion.Body>
-                          </Accordion.Item>
-                        </Accordion>
-                      </div>
-                    ))}
+                    {[...summonerInfo.match_summoner_performances]
+                      .reverse()
+                      .map((match_summoner_performance, index) => (
+                        <div key={match_summoner_performance.id}>
+                          <Accordion>
+                            <Accordion.Item eventKey={index}>
+                              <Accordion.Header> Match #{index + 1} </Accordion.Header>
+                              <Accordion.Body>
+                                <p> match id: {match_summoner_performance.match_id} </p>
+                                <p> summoner_info_id: {match_summoner_performance.summoner_info_id} </p>
+                                <p> riot_match_id: {match_summoner_performance.riot_match_id} </p>
+                                <p> puuid: {match_summoner_performance.puuid} </p>
+                                <p> gold_left: {match_summoner_performance.gold_left} </p>
+                                <p> last_round: {match_summoner_performance.last_round} </p>
+                                <p> level_placement: {match_summoner_performance.level_placement} </p>
+                                <p> players_eliminated: {match_summoner_performance.players_eliminated} </p>
+                                <p> time_eliminated: {match_summoner_performance.time_eliminated} </p>
+                                <p> total_damage_to_players: {match_summoner_performance.total_damage_to_players} </p>
+                                <p> first_augment: {match_summoner_performance.first_augment} </p>
+                                <p> second_augment: {match_summoner_performance.second_augment} </p>
+                                <p> third_augment: {match_summoner_performance.third_augment} </p>
+                                <p> companion_id: {match_summoner_performance.companion_id} </p>
+                              </Accordion.Body>
+                            </Accordion.Item>
+                          </Accordion>
+                        </div>
+                      ))}
                   </Accordion>
                   <p></p>
 
