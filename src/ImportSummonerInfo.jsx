cd8c225f1c522ca4c1d055098ac177cad56bc521
Paolo Maloles <jpaolomaloles@gmail.com>
2023-07-13 19:31:09 -0500

    coded tentative ImportSummonerInfo.jsx

--- src/ImportSummonerInfo.jsx
+++ src/ImportSummonerInfo.jsx
@@ -1,7 +1,24 @@
 export function ImportSummonerInfo() {
+  const handleOnSubmit = (event) => {
+    event.preventDefault();
+    const params = new FormData(event.target);
+    // props.handleCreateSummonerInfo(params, () => event.target.reset());
+    // use params.summoner_name and params.region_id in axios request
+  };
+
   return (
-    <>
-      <div>Import Summoner Info</div>
-    </>
+    <div>
+      <h1>Import Summoner Info</h1>
+      <form onSubmit={handleOnSubmit}>
+        <div>
+          region: <input name="region" type="text"></input>
+        </div>
+        <div>
+          summoner_name: <input name="summoner_name" type="text"></input>
+          {/* set this to lowercase in axios request */}
+        </div>
+        <button type="submit"> Import Summoner Info </button>
+      </form>
+    </div>
   );
 }
