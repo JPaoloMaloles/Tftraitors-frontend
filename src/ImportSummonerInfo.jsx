05cebdd55b8e2fc478e3b46b7b8dbce2dc54fbd6
Paolo Maloles <jpaolomaloles@gmail.com>
2023-07-16 17:09:08 -0500

    turned off autocomplete for region input for import summoner info

--- src/ImportSummonerInfo.jsx
+++ src/ImportSummonerInfo.jsx
@@ -44,6 +44,7 @@ export function ImportSummonerInfo(props) {
                 title="Must be BR1, EUN1, EUW1, JP1, KR, LA1, LA2, NA1, OC1, PH2, RU, SG2, TH2, TR1, TW2, or
     VN2, delete entry to have dropdown appear"
                 defaultValue="NA1"
+                autoComplete="off"
               />
               <datalist id="region_abbv">
                 <option value="BR1" />
